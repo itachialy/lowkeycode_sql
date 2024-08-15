ba889826c186ea495c4be99036d794672e2f63ca
itachialy <zassuo@gmail.com>
2024-08-15 15:31:36 +0700

    demo

---
+++ btvn2.sql
@@ -0,0 +1,32 @@
+select class_name as lop_hoc, count(student_id) as so_luong
+from student s
+inner join class c on s.class_id = c.class_id
+group by class_name;
+
+select class_name as lop_hoc, max(student_point) as diem_cao_nhat
+from student s
+inner join class c on s.class_id = c.class_id
+group by class_name;
+
+select class_name as lop_hoc, avg(student_point) as diem_trung_binh
+from student s
+inner join class c on s.class_id = c.class_id
+group by class_name;
+
+select student_name as name , student_birthday as birthday
+from student
+union 
+select  instructor_name as name , instructor_birthday as birthday
+from instructor;
+
+select student_name, student_point
+from student
+order by student_point desc
+limit 3;
+
+select student_name, student_point
+from student
+order by student_point desc
+limit 1;
+
+
--- demo_btvn.sql
+++ demo_btvn.sql
@@ -39,8 +39,7 @@ create table instructor_class(
     foreign key(instructor_id) references instructor(instructor_id)
 );
 
-insert into class (class_name) 
-values ('c1121g1'), ('c1221g1'),('a0821i1'),('a0921i1');
+insert into class (class_name) values ('c1121g1'), ('c1221g1'),('a0821i1'),('a0921i1');
 
 insert into jame_account(user_name,password)
  values('cunn','12345'),('chunglh','12345'),('hoanhh','12345'),('dungd','12345'),('huynhtd','12345'),
@@ -52,27 +51,72 @@ insert into instructor(instructor_name,instructor_birthday, instructor_salary)
 
  
  insert into student(student_name,student_birthday, student_gender,student_point, class_id,user_name) 
- values ('nguyen ngoc cu','1981-12-12',1,8,1,'cunn'),
- ('le hai chung','1981-12-12',1,5,1,'chunglh'),
- ('hoang huu hoan','1990-12-12',1,6,2,'hoanhh'),
- ('dau dung','1987-12-12',1,8,1,'dungd'),
- ('ta dinh huynh','1981-12-12',1,7,2,'huynhtd'),
- ('nguyen minh hai','1987-12-12',1,9,1,'hainm'),
- ('tran van nam','1989-12-12',1,4,2,'namtv'),
- ('vo minh hieu','1981-12-12',1,3,1,'hieuvm'),
- ('le xuan ky','1981-12-12',1,7,2,'kynx'),
- ('le minh vu','1981-12-12',1,7,1,'vulm'),
- ('nguyen van a','1981-12-12',1,8,null,'anv'),
- ('tran van b','1981-12-12',1,5,null,'bnv');
+ values ('nguyen ngoc cu','1981-12-12',1,8,1,'cunn'),('le hai chung','1981-12-12',1,5,1,'chunglh'),
+ ('hoang huu hoan','1990-12-12',1,6,2,'hoanhh'),('dau dung','1987-12-12',1,8,1,'dungd'),
+ ('ta dinh huynh','1981-12-12',1,7,2,'huynhtd'),('nguyen minh hai','1987-12-12',1,9,1,'hainm'),
+ ('tran van nam','1989-12-12',1,4,2,'namtv'),('vo minh hieu','1981-12-12',1,3,1,'hieuvm'),
+ ('le xuan ky','1981-12-12',1,7,2,'kynx'),('le minh vu','1981-12-12',1,7,1,'vulm'),
+ ('nguyen van a','1981-12-12',1,8,null,'anv'),('tran van b','1981-12-12',1,5,null,'bnv');
 
- insert into instructor_class(class_id,instructor_id) 
- values (1,1),(1,2),(2,1),(2,2),(3,1),(3,2);
+ insert into instructor_class(class_id,instructor_id) values (1,1),(1,2),(2,1),(2,2),(3,1),(3,2);
+
  -- 1. Lấy ra thông tin các học viên, và cho biết các học viên đang theo học lớp nào (sử dụng join).
+ select *
+ from student
+ inner join class on student.class_id = class.class_id;
+ 
  -- 2. Lấy ra thông tin các học viên, và cho biết các học viên đang theo học lớp nào và cả các bạn đã đăng ký nhưng chưa có lớp học.
+ select *
+ from student
+ left join class on student.class_id = class.class_id;
+
  -- 3. Lấy thông tin của các học viên tên “Hai” và 'Huynh’
+ select *
+ from student
+ where student_name like "% hai" or student_name like "% huynh";
+ 
+ 
  -- 4. Lấy ra học viên có điểm lớn hơn 5 .
+ select student.student_name 
+ from student
+ where student_point > 5;
+ 
+ 
  -- 5. Lấy ra học viên có họ là “nguyen”
+ select student.student_name
+ from student
+ where student_name like "nguyen%";
+ 
+ 
  -- 6. Thông kế số lượng học sinh theo từng loại điểm.
+ select student_point, count(student_point) as "so luong"
+ from student
+ group by student_point;
+ 
+ 
  -- 7. Thông kế số lượng học sinh theo điểm và điểm phải lớn hơn 5
+ select student_point, count(student_point) as "so luong"
+ from student 
+ where student_point > 5
+ group by student_point;
+ 
+ 
  -- 8. Thông kế số lượng học sinh theo điểm lớn hơn 5 và chỉ hiện thị với số lượng >= 2
+select student_point, count(*) as "so luong"
+from student 
+where student_point > 5
+group by student_point
+having count(*) >= 2;
+ 
  -- 9. Lấy ra danh sách học viên của lớp c1121g1 và sắp xếp tên học viên theo alphabet.
+ 
+ select s.student_name, c.class_name
+ from student s
+ inner join class c on s.class_id = c.class_id
+ where c.class_name = "c1121g1"
+ order by substring_index(s.student_name, ' ', -1) asc;
+ 
+ select distinct instructor_name,class_name
+ from class c
+ inner join instructor_class ic on c.class_id = ic.instructor_id
+ inner join instructor i on ic.class_id = i.instructor_id;
